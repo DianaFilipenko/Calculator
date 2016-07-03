216b877c9d489d701f709965c2438574a326b10b
DianaFilipenko <1998fda@mail.ru>
2016-07-03 15:50:40 +0500

    calculator

---
+++ App4/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App4
+{
+    [Activity(Label = "App4", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        //int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += new EventHandler(button_click);
+        }
+        void button_click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(this, typeof(EditText));
+            base.StartActivity(intent);
+        }
+    }
+}
+
